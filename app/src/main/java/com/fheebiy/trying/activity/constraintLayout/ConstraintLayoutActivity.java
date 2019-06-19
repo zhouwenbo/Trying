7d178ed3059f9de1c3f921679bd19811c2743182
周文波 <zhouwenbo03@meituan.com>
2019-06-19 14:55:46 +0800

    [ADD]: ConstraintLayout用法学习。

---
+++ app/src/main/java/com/fheebiy/trying/activity/constraintLayout/ConstraintLayoutActivity.java
@@ -0,0 +1,23 @@
+package com.fheebiy.trying.activity.constraintLayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fheebiy.trying.R;
+
+/**
+ * Created on 2019/6/19.
+ *
+ * @author bob zhou.
+ * Description:
+ */
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
